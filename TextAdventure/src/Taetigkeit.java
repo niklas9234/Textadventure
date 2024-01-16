580accaa838f86817fbb38f70e3a986c7a0b681a
niklas9234 <153933597+niklas9234@users.noreply.github.com>
2024-01-16 15:14:35 +0100

    Delete TextAdventure directory

--- TextAdventure/src/Taetigkeit.java
+++
@@ -1,9 +0,0 @@
-public class Taetigkeit {
-    final private int dauer;
-    final private int lohn;
-
-    public Taetigkeit(int dauer, int lohn) {
-        this.dauer = dauer;
-        this.lohn = lohn;
-    }
-}
